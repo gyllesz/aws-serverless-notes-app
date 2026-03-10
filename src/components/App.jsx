07619913433fc293ee952b3a8765ac8aaa1b80cd
Gylles Varga <149961053+gyllesz@users.noreply.github.com>
2026-03-11 03:58:41 +1100

    Add AWS serverless notes app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,35 +1,91 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 function App() {
   const [notes, setNotes] = useState([]);
 
-  function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
-    });
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    try {
+      const response = await fetch(`${API_URL}/notes`);
+      const data = await response.json();
+
+      if (response.ok) {
+        setNotes(data);
+      } else {
+        console.error("Failed to fetch notes:", data);
+      }
+    } catch (error) {
+      console.error("Error calling GET /notes:", error);
+    }
   }
 
-  function deleteNote(id) {
-    setNotes(prevNotes => {
-      return prevNotes.filter((noteItem, index) => {
-        return index !== id;
+  async function addNote(newNote) {
+    try {
+      const response = await fetch(`${API_URL}/notes`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          title: newNote.title,
+          content: newNote.content
+        })
       });
-    });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setNotes(prevNotes => {
+          return [...prevNotes, data.note];
+        });
+      } else {
+        console.error("Failed to save note:", data);
+      }
+    } catch (error) {
+      console.error("Error calling POST /notes:", error);
+    }
+  }
+
+  async function deleteNote(noteId) {
+    try {
+      const response = await fetch(`${API_URL}/notes/${noteId}`, {
+        method: "DELETE"
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setNotes(prevNotes => {
+          return prevNotes.filter(noteItem => {
+            return noteItem.noteId !== noteId;
+          });
+        });
+      } else {
+        console.error("Failed to delete note", data);
+      }
+    } catch (error) {
+      console.error("Error calling DELETE API:", error);
+    }
   }
 
   return (
     <div>
       <Header />
       <CreateArea onAdd={addNote} />
-      {notes.map((noteItem, index) => {
+      {notes.map(noteItem => {
         return (
           <Note
-            key={index}
-            id={index}
+            key={noteItem.noteId}
+            id={noteItem.noteId}
             title={noteItem.title}
             content={noteItem.content}
             onDelete={deleteNote}
